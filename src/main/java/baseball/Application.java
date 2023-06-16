c03c76a12a041e345ff08f3f0cca55b883c24e8a
howudong <53307093+howudong@users.noreply.github.com>
2023-06-17 01:31:11 +0900

    feat : 정답 랜덤 생성 및 사용자 입력 기능 추가
    InitGame()
    => 게임 시작 문구 및 SetAnswerNumber() 함수 호출
    
    SetAnswerNumber()
    => 정답 숫자 생성 (난수 생성)
    
    GetInputNumber()
    => 사용자의 숫자 입력을 받는 함수.
    - 예외처리, 스트라이크 볼 판단, 기능 추가 필요

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
+
+
+    static List<Integer> answerNum = new ArrayList<>();
+    public static void InitGame(){
+        answerNum.clear();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        SetAnswerNumber();
+
+    }
+    public static void SetAnswerNumber(){
+        while(answerNum.size() <= 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!answerNum.contains(randomNumber)){
+                answerNum.add(randomNumber);
+            }
+        }
+    }
+    public static List<Integer> GetInputNumber(){
+        System.out.print("숫자를 입력해주세요 :");
+        String inputNumber = Console.readLine();
+
+        return inputNumber.chars().mapToObj(e -> e-'0').collect(Collectors.toList());
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InitGame();
+        List<Integer> InputNumber = GetInputNumber();
+
+
     }
 }
