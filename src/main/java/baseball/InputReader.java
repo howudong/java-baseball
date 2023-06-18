aafb8d43e91b73ca3d5c8919a1c129ae5eb93ff0
howudong <53307093+howudong@users.noreply.github.com>
2023-06-18 23:57:02 +0900

    refactor($InputReader) : change Method Name
    - change Method getInputNumberList() -> inputNumberList()
    - change Method getInputNumber() -> inputEndSignal()

--- src/main/java/baseball/InputReader.java
+++ src/main/java/baseball/InputReader.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputReader {
-    public List<Integer> getInputNumberList() {
+    public List<Integer> inputNumberList() {
         System.out.print("숫자를 입력해주세요. : ");
         String inputNumber = Console.readLine();
 
         return inputNumber.chars().mapToObj(e -> e - '0').collect(Collectors.toList());
     }
 
-    public int getInputNumber() {
+    public int inputEndSignal() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String inputNumber = Console.readLine();
 
