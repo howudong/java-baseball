509eaff340eda427860d4ecd647964392724f644
howudong <53307093+howudong@users.noreply.github.com>
2023-06-18 23:54:18 +0900

    feat($Application) : add game flow (from start to finish)
    - Add EndFlag to Finish Program
    - Make do while{} loop to progress Game
    - Added Finish Text

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Application {
+    static final String END_FLAG = "3스트라이크";
 
+    static List<Integer> answerNumber = new ArrayList<>();
+    static InputReader reader = new InputReader();
 
-    static List<Integer> answerNumber;
     public static void initGame() {
         if (answerNumber.isEmpty()) {
             System.out.println("숫자 야구 게임을 시작합니다.");
@@ -18,20 +19,24 @@ public class Application {
         answerNumber = new SetAnswerNum().getAnswerNum();
     }
 
-    public static void playGame(){
-        InputReader reader = new InputReader();
+    public static void playGame() {
         NumberChecker check = new NumberChecker(answerNumber);
 
-        while(!check.countBaseBall(reader.getInputNumberList()).equals("4 Strikes")){
+        List<Integer> inputNumberList = new ArrayList<>();
+        String matchResult;
+        do {
+            inputNumberList = reader.inputNumberList();
+            matchResult = check.getStrikeAndBall(inputNumberList);
+            System.out.println(matchResult);
+        } while (!matchResult.equals(END_FLAG));
 
-        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public static void main(String[] args) {
+        do {
             initGame();
             playGame();
-
-
-
+        } while (reader.inputEndSignal() != 2);
     }
 }
