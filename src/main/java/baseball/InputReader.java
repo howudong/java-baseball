9f720bf697a6d62b291b9d0c85b1671880ca6dcc
howudong <53307093+howudong@users.noreply.github.com>
2023-06-18 19:03:43 +0900

    feat($location) : Add 'InputRead.java' class
    - To manage input system easily, Add InputRead.java
    - This class can convert inputs(String) to list(integer) or int

---
+++ src/main/java/baseball/InputReader.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputReader {
+    public List<Integer> getInputNumberList() {
+        System.out.print("숫자를 입력해주세요. : ");
+        String inputNumber = Console.readLine();
+
+        return inputNumber.chars().mapToObj(e -> e - '0').collect(Collectors.toList());
+    }
+
+    public int getInputNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String inputNumber = Console.readLine();
+
+        return Integer.parseInt(inputNumber);
+    }
+
+
+}
