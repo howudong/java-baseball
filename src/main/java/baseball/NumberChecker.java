69b8987da2c15be151e55bc8c91b7e61457d2955
howudong <53307093+howudong@users.noreply.github.com>
2023-06-19 00:03:28 +0900

    feat($NumberChecker) : add Checking Strike and Ball System
    - getStrike() : count amount of strikes
    - getBall() : count amount of balls
    - resultToString() : Covert Result to String

--- src/main/java/baseball/NumberChecker.java
+++ src/main/java/baseball/NumberChecker.java
@@ -1,14 +1,50 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberChecker {
-    List<Integer> answerNum;
-    public NumberChecker(List<Integer> answerNum){
+    private List<Integer> answerNum = new ArrayList<>();
+    private int ball;
+    private int strike;
+
+    public NumberChecker(List<Integer> answerNum) {
         this.answerNum = answerNum;
+        ball = 0;
+        strike = 0;
     }
 
-    public String countBaseBall(List<Integer> inputNum){
+    private void getStrike(List<Integer> inputNum, int idx) {
+        if (answerNum.get(idx).equals(inputNum.get(idx))) {
+            strike++;
+        }
+    }
+
+    private void getBall(List<Integer> inputNum, int idx) {
+        int num = inputNum.get(idx);
+        if (!answerNum.get(idx).equals(num) && answerNum.contains(num)) {
+            ball++;
+        }
+    }
+
+    public String getStrikeAndBall(List<Integer> inputNum) {
+        for (int i = 0; i < inputNum.size(); i++) {
+            getStrike(inputNum, i);
+            getBall(inputNum, i);
+        }
+
+        return resultToString(ball, strike);
+    }
+
+    private String resultToString(int ball, int strike) {
+        if (ball == 0 && strike == 0) return "낫싱";
+        if(ball == 0){
+            return strike+"스트라이크";
+        }
+        if(strike == 0){
+            return ball+"볼";
+        }
+        return ball+"볼 " +strike+"스트라이크";
     }
 
 }
