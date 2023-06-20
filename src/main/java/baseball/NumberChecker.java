89d9949d5c146db02950bf1a48b5f414dd0212c8
howudong <53307093+howudong@users.noreply.github.com>
2023-06-20 14:03:04 +0900

    fix($NumberChecker) : fix that getStrikeAndBall() returns incorrect value
    - added printResult() method that print getStrikeAndBall
    this method was created to prevent printing when getStrikeAndBall is null
    
    - remove static variable strike,ball
    now that variable is local variable

--- src/main/java/baseball/NumberChecker.java
+++ src/main/java/baseball/NumberChecker.java
@@ -5,46 +5,64 @@ import java.util.List;
 
 public class NumberChecker {
     private List<Integer> answerNum = new ArrayList<>();
-    private int ball;
-    private int strike;
 
     public NumberChecker(List<Integer> answerNum) {
         this.answerNum = answerNum;
-        ball = 0;
-        strike = 0;
     }
 
-    private void getStrike(List<Integer> inputNum, int idx) {
-        if (answerNum.get(idx).equals(inputNum.get(idx))) {
-            strike++;
-        }
-    }
-
-    private void getBall(List<Integer> inputNum, int idx) {
-        int num = inputNum.get(idx);
-        if (!answerNum.get(idx).equals(num) && answerNum.contains(num)) {
-            ball++;
+    public void printResult(String resultWord){
+        String printWord = getStrikeAndBall(printWord);
+        if(printWord != null){
+            System.out.println(printWord);
         }
     }
 
     public String getStrikeAndBall(List<Integer> inputNum) {
+        if (inputNum == null) {
+            return null;
+        }
+
+        int strike = 0;
+        int ball = 0;
         for (int i = 0; i < inputNum.size(); i++) {
-            getStrike(inputNum, i);
-            getBall(inputNum, i);
+            strike += getStrike(inputNum, i);
+            ball += getBall(inputNum, i);
         }
 
         return resultToString(ball, strike);
     }
 
+
+    private int getStrike(List<Integer> inputNum, int idx) {
+        int strike = 0;
+        if (answerNum.get(idx).equals(inputNum.get(idx))) {
+            strike++;
+        }
+        return strike;
+    }
+
+    private int getBall(List<Integer> inputNum, int idx) {
+        int ball = 0;
+        int num = inputNum.get(idx);
+        if (!answerNum.get(idx).equals(num) && answerNum.contains(num)) {
+            ball++;
+        }
+        return ball;
+    }
+
+
+
+
+
     private String resultToString(int ball, int strike) {
         if (ball == 0 && strike == 0) return "낫싱";
-        if(ball == 0){
-            return strike+"스트라이크";
+        if (ball == 0) {
+            return strike + "스트라이크";
         }
-        if(strike == 0){
-            return ball+"볼";
+        if (strike == 0) {
+            return ball + "볼";
         }
-        return ball+"볼 " +strike+"스트라이크";
+        return ball + "볼 " + strike + "스트라이크";
     }
 
 }
